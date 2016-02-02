f02b7b72b6dabe3e17550d6eadf9ff864cbfd081
Yiyi <yiyiwzhang@gmail.com>
2016-02-01 22:35:35 -0500

    made test topdown game asteroids

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// PlayerController takes in key inputs and moves the game object accordingly
+/// </summary>
+
+public class PlayerController : MonoBehaviour {
+
+	public Rigidbody2D rbody;//rigidbodies control physics, if you make it public, it appears in inspector!!!
+	float speed = 20f; //float is for decimals
+
+	string upButton = "up"; //buttons are string variables
+	string downButton = "down";
+	string leftButton = "left";
+	string rightButton = "right";
+	
+	// Update is called once per frame
+	void Update(){		
+		if (Input.GetKeyDown (upButton)) {
+			rbody.AddForce (new Vector2 (0f, speed));
+		}
+
+		if (Input.GetKeyDown (downButton)) {
+			rbody.AddForce (new Vector2 (0f, -speed));
+		}
+
+		if (Input.GetKeyDown (leftButton)) {
+			rbody.AddForce (new Vector2 (-speed, 0f));
+		}
+			
+		if (Input.GetKeyDown (rightButton)) {
+			rbody.AddForce (new Vector2 (speed, 0f));
+		}
+	}
+}
---
+++ Assets/Scripts/SpaceshipController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceshipController : MonoBehaviour {
+
+	public Rigidbody2D spaceshipbody;//rigidbodies control physics, if you make it public, it appears in inspector!!!
+
+	string upButton = "up";
+	string leftButton = "left";
+	string rightButton = "right";
+	string downButton = "down";
+
+	float speed = 2f;
+
+	// Use this for initialization
+	void Start () {
+		spaceshipbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (upButton)) {
+			spaceshipbody.AddForce (transform.up * speed);
+		}
+
+		if (Input.GetKeyDown (leftButton)) {
+			spaceshipbody.AddTorque (20 * speed);
+		}
+
+		if (Input.GetKeyDown (rightButton)) {
+			spaceshipbody.AddTorque (-20 * speed);
+		}
+
+		if (Input.GetKeyDown (downButton)) {
+			spaceshipbody.AddForce (-transform.up * speed);
+		}
+	}
+}
